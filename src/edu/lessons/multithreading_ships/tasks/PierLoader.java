1066bfc0b2ae9c933dacb2ceac8475f8b8b7e611
MelnykVL <w.vladyslavmelnyk@gmail.com>
2021-07-20 10:20:19 +0300

    multithreading_ships is done

--- src/edu/lessons/multithreading_ships/tasks/PierLoader.java
+++ src/edu/lessons/multithreading_ships/tasks/PierLoader.java
@@ -1,4 +1,39 @@
 package edu.lessons.multithreading_ships.tasks;
 
-public class PierLoader {
+import edu.lessons.multithreading_ships.Tunnel;
+import edu.lessons.multithreading_ships.ships.Ship;
+import edu.lessons.multithreading_ships.ships.types.Type;
+
+public class PierLoader implements Runnable {
+
+    private Tunnel tunnel;
+    private Type shipType;
+
+    public PierLoader(Tunnel tunnel, Type shipType) {
+        this.tunnel = tunnel;
+        this.shipType = shipType;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+
+                Thread.currentThread().setName("Loader " + shipType);
+                Ship ship = tunnel.get(shipType);
+                if (ship != null)
+                    while (ship.countCheck()) {
+                        Thread.sleep(100);
+                        ship.add(10);
+                        System.out.println(ship.getCount() + " Loaded ship. " + Thread.currentThread().getName());
+                    }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
 }
--- src/edu/lessons/multithreading_ships/tasks/ShipGenerator.java
+++ src/edu/lessons/multithreading_ships/tasks/ShipGenerator.java
@@ -1,4 +1,49 @@
 package edu.lessons.multithreading_ships.tasks;
 
-public class ShipGenerator {
+import edu.lessons.multithreading_ships.Tunnel;
+import edu.lessons.multithreading_ships.ships.Ship;
+import edu.lessons.multithreading_ships.ships.types.Size;
+import edu.lessons.multithreading_ships.ships.types.Type;
+
+import java.util.Random;
+
+public class ShipGenerator implements Runnable {
+
+    private Tunnel tunnel;
+    private int shipCount;
+
+    public ShipGenerator(Tunnel tunnel, int shipCount) {
+        this.tunnel = tunnel;
+        this.shipCount = shipCount;
+    }
+
+    @Override
+    public void run() {
+
+        int count = 0;
+        while (count < shipCount) {
+
+            Thread.currentThread().setName(" Ship generator");
+            count++;
+            tunnel.add(new Ship(getRandomSize(), getRandomType()));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    private Size getRandomSize() {
+        Random random = new Random();
+        return Size.values()[random.nextInt(Size .values().length)];
+    }
+
+    private Type getRandomType() {
+        Random random = new Random();
+        return Type.values()[random.nextInt(Type.values().length)];
+    }
+
 }
